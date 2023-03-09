87c0716956af46bfc67591a80ad5d392c656bac1
ABU KALAM <99273096+Abukalam-web@users.noreply.github.com>
2023-03-09 06:50:09 +0600

    djhfj

--- script.js
+++ script.js
@@ -39,3 +39,60 @@ fullArea.addEventListener("click", function(){
 
 
 
+const btnDeposit = document.getElementById("depositBtn");
+
+btnDeposit.addEventListener("click", function(){
+    const depoInput = document.getElementById("depositInput").value;
+    const depoNumber =parseFloat(depoInput);
+
+    const currentDepo = document.getElementById("depositAmount").innerText;
+    const currentAmountNumber = parseFloat(currentDepo);
+
+    const totalDepo = depoNumber + currentAmountNumber;
+
+    document.getElementById("depositAmount").innerText = totalDepo;
+
+    const addBalance = document.getElementById("balanceAmount").innerText;
+    const addBalanceNumber = parseFloat(addBalance);
+
+    const subAddTotal = addBalanceNumber + depoNumber;
+
+    document.getElementById("balanceAmount").innerText = subAddTotal;
+});
+
+// withdraw er jonno
+
+const withdrawBtn = document.getElementById("wBtn");
+
+withdrawBtn.addEventListener("click", function(){
+    const wInput = document.getElementById("wInput").value;
+    const wInputNumber = parseFloat(wInput);
+
+    const wDisplay = document.getElementById("withdrawAmount").innerText;
+    const wDisplayNumber = parseFloat(wDisplay);
+    const totalW = wInputNumber  + wDisplayNumber;
+
+    document.getElementById("withdrawAmount").innerText = totalW;
+
+
+    const balanceDisplay = document.getElementById("balanceAmount").innerText;
+    const balanceDisplayNumber = parseFloat(balanceDisplay);
+
+    const semiTotal = balanceDisplayNumber - wInputNumber;
+    if(semiTotal <0)
+    {
+        alert("Your Balance Is Unsufficient")
+        
+    }
+
+    
+
+    document.getElementById("balanceAmount").innerText = semiTotal;
+
+
+
+});
+
+
+
+
